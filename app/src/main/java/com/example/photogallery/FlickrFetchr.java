cb1cf05b241e43fbcb44a42d389cf3150221ddff
uran23564 <uran23564@gmail.com>
2017-06-06 12:28:29 +0200

    asdf

--- app/src/main/java/com/example/photogallery/FlickrFetchr.java
+++ app/src/main/java/com/example/photogallery/FlickrFetchr.java
@@ -19,6 +19,8 @@ public class FlickrFetchr{
     private static final String TAG="FlickrFetchr";
     private static final String API_KEY="0363ad859a7ae1001843f4baa4a865ce";
 
+    private List<GalleryItem> mItems=new ArrayList<>();
+    
     public byte[] getUrlBytes(String urlSpec) throws IOException{ // erzeugt einen rohes URL-Objekt (als Byte-Array) aus dem uebergebenen String
         URL url=new URL(urlSpec);
         HttpURLConnection connection = (HttpURLConnection)url.openConnection(); // erzeugt ein Connection-Objekt, das auf die URL zeigt. Dabei wird das URLConnection-Objekt zu einem spezifischen HttpURLConnection-Objekt gecastet (hat eigene interfaces, methoden etc.)
@@ -45,9 +47,32 @@ public class FlickrFetchr{
         return new String(getUrlBytes(urlSpec));
     }
 
-    public List<GalleryItem> fetchItems(){ // baut die entsprechende Request-URL zusammen und laedt die dazugehoerigen daten runter
-        List<GalleryItem> items=new ArrayList<>();
-
+//     public List<GalleryItem> fetchItems(){ // baut die entsprechende Request-URL zusammen und laedt die dazugehoerigen daten runter
+//         // List<GalleryItem> items=new ArrayList<>();
+// 
+//         try{
+//             String url= Uri.parse("https://api.flickr.com/services/rest").buildUpon()
+//                     .appendQueryParameter("method", "flickr.photos.getRecent")
+//                     .appendQueryParameter("api_key", API_KEY)
+//                     .appendQueryParameter("format", "json")
+//                     .appendQueryParameter("nojsoncallback", "1")
+//                     .appendQueryParameter("extras", "url_s") // sagt Flickr, dass wir auch dir url fuer die kleine version des bildes haben wollen (wenn sie existiert)
+//                     .build().toString();
+//             String jsonString = getUrlString(url);
+//             Log.i(TAG, "Received JSON: " + jsonString);
+//             JSONObject jsonBody = new JSONObject(jsonString); // json-konstruktor uebersetzt die json-hierarchie, die im string abgespeichert ist, in ein entsprechendes java-objekt mit der selben hierarchie
+//             // hiert ist jsonBody das top-level-Objekt, das das JSONArray photo traegt, welches wiederum eine Familie von JSON-Objekten beinhaltet. jedes davon stellt die metadaten eines einzelnen fotos dar
+//             parseItems(mItems,jsonBody);
+//         } catch(IOException ioe){
+//             Log.e(TAG, "Failed to fetch items", ioe);
+//         } catch(JSONException je){
+//             Log.e(TAG, "Failed to parse JSON", je);
+//         }
+// 
+//         return mItems;
+//     }
+    
+    public List<GalleryItem> fetchItemsFromPage(int page){
         try{
             String url= Uri.parse("https://api.flickr.com/services/rest").buildUpon()
                     .appendQueryParameter("method", "flickr.photos.getRecent")
@@ -55,19 +80,20 @@ public class FlickrFetchr{
                     .appendQueryParameter("format", "json")
                     .appendQueryParameter("nojsoncallback", "1")
                     .appendQueryParameter("extras", "url_s") // sagt Flickr, dass wir auch dir url fuer die kleine version des bildes haben wollen (wenn sie existiert)
+                    .appendQueryParameter("page", String.valueOf(page))
                     .build().toString();
             String jsonString = getUrlString(url);
             Log.i(TAG, "Received JSON: " + jsonString);
             JSONObject jsonBody = new JSONObject(jsonString); // json-konstruktor uebersetzt die json-hierarchie, die im string abgespeichert ist, in ein entsprechendes java-objekt mit der selben hierarchie
             // hiert ist jsonBody das top-level-Objekt, das das JSONArray photo traegt, welches wiederum eine Familie von JSON-Objekten beinhaltet. jedes davon stellt die metadaten eines einzelnen fotos dar
-            parseItems(items,jsonBody);
+            parseItems(mItems,jsonBody);
         } catch(IOException ioe){
             Log.e(TAG, "Failed to fetch items", ioe);
         } catch(JSONException je){
             Log.e(TAG, "Failed to parse JSON", je);
         }
 
-        return items;
+        return mItems;
     }
 
     private void parseItems(List<GalleryItem> items, JSONObject jsonBody) throws IOException, JSONException{ // durchsucht das photos-array des json-objekts photo aus dem ober-json-objekt und steckt jedes foto
@@ -75,7 +101,7 @@ public class FlickrFetchr{
         JSONObject photosJsonObject=jsonBody.getJSONObject("photos");
         JSONArray photoJsonArray=photosJsonObject.getJSONArray("photo");
 
-        for(int i=0;i<photoJsonArray.length();i++){
+        for(int i=items.size();i<photoJsonArray.length()+items.size();i++){
             JSONObject photoJsonObject= photoJsonArray.getJSONObject(i);
 
             GalleryItem item= new GalleryItem();
--- app/src/main/java/com/example/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/example/photogallery/PhotoGalleryFragment.java
@@ -24,10 +24,11 @@ import java.util.List;
 
 public class PhotoGalleryFragment extends Fragment {
     private static final String TAG="PhotoGalleryFragment";
-    private static final int REQUEST_PERMISSION = 0;
 
     private RecyclerView mPhotoRecyclerView;
     private List<GalleryItem> mItems=new ArrayList<>();
+    // private RecyclerView.OnScrollListener mScrollListener;
+    private int pages=1; // wir starten mit der ersten seite
 
     public static PhotoGalleryFragment newInstance(){
         return new PhotoGalleryFragment();
@@ -37,7 +38,7 @@ public class PhotoGalleryFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
-        new FetchItemsTask().execute(); // startet den async-task
+        new FetchItemsTask().execute(pages); // startet den async-task
     }
 
     @Override
@@ -45,12 +46,23 @@ public class PhotoGalleryFragment extends Fragment {
         View v=inflater.inflate(R.layout.fragment_photo_gallery,container,false);
         mPhotoRecyclerView=(RecyclerView) v.findViewById(R.id.photo_recycler_view);
         mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(),3));
-
-        int permissionCheck = ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_CONTACTS);
-        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
-            PhotoGalleryFragment.this.requestPermissions(new String[]{Manifest.permission.INTERNET}, REQUEST_PERMISSION); // frage user, ob internet benutzt werden darf
-        }
+        // mPhotoRecyclerView.addOnScrollListener(mScrollListener);
+        mPhotoRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener{
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState){
+                if(newState==SCROLL_STATE_SETTLING){
+                    pages++;
+                    FetchItemsTask().execute(pages);
+                }
+            }
+            
+            @Override void onScrolled(RecyclerView recyclerView, int dx, int dy){
+                // TODO
+            }
+        });
+        
         setupAdapter();
+        
         return v;
     }
 
@@ -100,11 +112,12 @@ public class PhotoGalleryFragment extends Fragment {
     }
 
 
-    private class FetchItemsTask extends AsyncTask<Void,Void,List<GalleryItem>> { // bilder werden in einem neuen thread heruntergeladen -- dazu verwendenen wir die AsyncTask-Klasse
+    private class FetchItemsTask extends AsyncTask<int,Void,List<GalleryItem>> { // bilder werden in einem neuen thread heruntergeladen -- dazu verwendenen wir die AsyncTask-Klasse
+        // erster parameter gibt typ der parameter an, die beim aufruf von AsyncTask.execute() uebergeben werden
         // dritter parameter oben gibt den typ des resultats von doInBackground an. dies ist automatisch auch der typ, der als argument in onPostExecute verwendet werden muss
         @Override
-        protected List<GalleryItem> doInBackground(Void... params){ // das zeug laeuft im hintergrund ab
-            return new FlickrFetchr().fetchItems();
+        protected List<GalleryItem> doInBackground(Int... params){ // das zeug laeuft im hintergrund ab
+            return new FlickrFetchr().fetchItemsFromPage(params);
         }
 
         @Override
